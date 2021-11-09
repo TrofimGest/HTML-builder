8b6ab344225295efef07ceafbfce96e90f7317e7
TrofimGest <gubaoslika@gmail.com>
2021-11-09 12:47:49 +0300

    fix: fix 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,8 +4,8 @@ fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
   console.log('Directory created and copied successfully!');
 });
 fs.readdir((path.join(__dirname, 'files')),{withFileTypes: true}, function(err, items) {
-  for (let i = items.length; i>0; i--) {
+  for (let i = 0 ; i<items.length; i++) {
     console.log(items[i]);
-    fs.createReadStream((path.join(__dirname, 'files', `${items[i].name}`))).pipe(fs.createWriteStream((path.join(__dirname, 'files-copy', `${items[i].name}`))));
+    fs.createReadStream((path.join(__dirname, 'files', items[i].name))).pipe(fs.createWriteStream((path.join(__dirname, 'files-copy', `${items[i].name}`))));
   }
 });
