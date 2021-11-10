7e6764d84fda939f3a2c5f269f4f4c52222b4a15
TrofimGest <gubaoslika@gmail.com>
2021-11-10 21:34:14 +0300

    fix: fix minor bugs

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,26 @@
 const path = require('path');
 const fs = require('fs');
-fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
-  console.log('Directory created and copied successfully!');
-});
-fs.readdir((path.join(__dirname, 'files')),{withFileTypes: true}, function(err, items) {
-  for (let i = 0 ; i<items.length; i++) {
-    console.log(items[i]);
-    fs.createReadStream((path.join(__dirname, 'files', items[i].name))).pipe(fs.createWriteStream((path.join(__dirname, 'files-copy', `${items[i].name}`))));
+fs.access(path.join(__dirname, 'files-copy'), (err) =>{
+  if (err){
+    fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
+      fs.readdir((path.join(__dirname, 'files')),{withFileTypes: true}, function(err, items) {
+        for (let i = 0 ; i<items.length; i++) {
+          fs.createReadStream((path.join(__dirname, 'files', items[i].name))).pipe(fs.createWriteStream((path.join(__dirname, 'files-copy', `${items[i].name}`))));
+          console.log('Directory created and copied successfully!');
+        }
+      });
+    });
+  }
+  else{
+    fs.rmdir(path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
+      fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, () => {
+        fs.readdir((path.join(__dirname, 'files')),{withFileTypes: true}, function(err, items) {
+          for (let i = 0 ; i<items.length; i++) {
+            fs.createReadStream((path.join(__dirname, 'files', items[i].name))).pipe(fs.createWriteStream((path.join(__dirname, 'files-copy', `${items[i].name}`))));
+            console.log('Directory created and copied successfully!');
+          }
+        });
+      });
+    });
   }
 });
